f02b8844fef9abec48da388d0ce7dc51b4b7d3f3
shivamThakurXcode <shivam.thakur.official40@gmail.com>
2025-03-27 12:19:53 +0530

    all page header ribbon set , create header.php file to all

---
+++ footer.php
@@ -0,0 +1,135 @@
+<footer class="footer">
+    <!-- Footer top -->
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-xl-4 me-5 col-md-6">
+                    <!-- Brand -->
+                    <div class="footer-widget">
+                        <a href="index.php" class="brand-img d-flex">
+                            <h4 class="h2">River Tern Lodge Bhadra</h4>
+                        </a>
+                        <p class="brand-desc text-white">Reconnect with nature at Rivertern Lodge, Bhadra. Peaceful surroundings, exciting adventures, and cozy comfort—your perfect escape is here.</p>
+                        <h4 class="text-white">Secure Online Booking</h4>
+                        <ul class="social-list">
+                            <li class="social-item">
+                                <img width="100%" height="100%" sizes="(max-width: 600px) 480px, (max-width: 992px) 768px, 1200px" src="assets/img/FooteSVG/paypal.svg" alt="payment-icons">
+                            </li>
+                            <li class="social-item">
+                                <img width="100%" height="100%" sizes="(max-width: 600px) 480px, (max-width: 992px) 768px, 1200px" src="assets/img/FooteSVG/mastercard.svg" alt="payment-icons">
+                            </li>
+                            <li class="social-item">
+                                <img width="100%" height="100%" sizes="(max-width: 600px) 480px, (max-width: 992px) 768px, 1200px" src="assets/img/FooteSVG/visa.svg" alt="payment-icons">
+                            </li>
+                            <li class="social-item">
+                                <img width="100%" height="100%" sizes="(max-width: 600px) 480px, (max-width: 992px) 768px, 1200px" src="assets/img/FooteSVG/american-express.svg" alt="payment-icons">
+                            </li>
+                            <li class="social-item">
+                                <img width="100%" height="100%" sizes="(max-width: 600px) 480px, (max-width: 992px) 768px, 1200px" src="assets/img/FooteSVG/rupay.svg" alt="payment-icons">
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /Brand -->
+                </div>
+
+                <div class="col-12 col-xl-2 col-md-6">
+                    <!-- Quick Links -->
+                    <div class="footer-widget">
+                        <h4 class="h4 text-white pb-2">Navigation</h4>
+                        <ul class="footer-link">
+                            <li class="link-item">
+                                <a href="about.php">About River Tern</a>
+                            </li>
+                            <li class="link-item">
+                                <a href="gallery.php">Photo Gallery</a>
+                            </li>
+                            <li class="link-item">
+                                <a href="things-to-do.php">Things To Do </a>
+                            </li>
+                            <li class="link-item">
+                                <a href="reach.php">How To Reach</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /Quick Links -->
+                </div>
+                <div class="col-12 col-xl-2 me-5 col-md-6">
+                    <!-- Quick Links -->
+                    <div class="footer-widget">
+                        <h4 class="h4 text-white pb-2">Accommodation</h4>
+                        <ul class="footer-link">
+                            <li class="link-item">
+                                <a href="loghut.php">Loghut Package</a>
+                            </li>
+                            <li class="link-item">
+                                <a href="cottage.php">Cottage Package</a>
+                            </li>
+                            <li class="link-item">
+                                <a href="contact-us.php">Contact Us</a>
+                            </li>
+                            <li class="link-item">
+                                <a href="cancellation.php">Cancellation Policy</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /Quick Links -->
+                </div>
+                <div class="col-12 col-xl-3 col-md-6">
+                    <div class="footer-widget">
+                        <h4 class="h4 text-white pb-2"> Get In Touch </h4>
+
+                        <div class="footer-local">
+                            <div class="col-1">
+                                <i class="hicon hicon-map-entry"></i>
+                            </div>
+                            <div class="col-10">
+                                <p class="text-white">Rivertern Lodge Bhadra, <br>
+                                    Behind Ranganathaswamy <br>
+                                    Temple, Tarikere, Lakkavalli,<br>
+                                    Karnataka 577115</p>
+                            </div>
+                        </div>
+
+                        <div class="footer-local">
+                            <div class="col-1 footer-contact-main">
+                                <i class="hicon hicon-telephone"></i>
+                                <i class="hicon hicon-email-envelope"></i>
+                            </div>
+                            <div class="footer-contact col-8">
+                                <a href="tel:+9480022108">+91-9480022108</a>
+                                <a href="mailto: book@riverternlodgebhadra.com"> book@riverternlodgebhadra.com</a>
+                            </div>
+                        </div>
+                        <!-- /Social -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /Footer top -->
+
+    <!-- Footer Bottom -->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <p class="mb-lg-0">© <span class="me-2" id="currentYear"></span>
+                        <span id="footer-mark"><a href="index.php">RiverternLodgeBhadra.com.</a></span> All rights reserved.
+                    </p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="text-start text-md-end">
+                        <ul class="list-inline mb-lg-0">
+                            <li class="list-inline-item">
+                                <a href="terms.php">Terms and Conditions</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="privacy.php">Privacy Policy</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
